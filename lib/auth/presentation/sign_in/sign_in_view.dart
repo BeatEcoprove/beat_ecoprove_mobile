17c849d54bd5a0e4c65fffd020584aba47fba068
Diogo Assunção <dassuncao1234@gmail.com>
2023-10-15 13:24:02 +0100

    add: Third Stage

--- lib/auth/presentation/sign_in/sign_in_view.dart
+++ lib/auth/presentation/sign_in/sign_in_view.dart
@@ -1,4 +1,4 @@
-import 'package:beat_ecoprove/auth/presentation/sign_in/stages/second_stage.dart';
+import 'package:beat_ecoprove/auth/presentation/sign_in/stages/third_stage.dart';
 import 'package:flutter/material.dart';
 
 class SignInView extends StatelessWidget {
@@ -8,6 +8,6 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SecondStage();
+    return const ThirdScene();
   }
 }
---
+++ lib/auth/presentation/sign_in/stages/third_stage.dart
@@ -0,0 +1,62 @@
+import 'package:beat_ecoprove/auth/widgets/scroll_handler.dart';
+import 'package:beat_ecoprove/core/config/global.dart';
+import 'package:beat_ecoprove/core/widgets/formated_button.dart';
+import 'package:beat_ecoprove/core/widgets/formatted_text_field/formated_text_field.dart';
+import 'package:beat_ecoprove/core/widgets/step_by_step.dart';
+import 'package:flutter/material.dart';
+
+class ThirdScene extends StatelessWidget {
+  static const double textFieldsGap = 26;
+
+  const ThirdScene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ScrollHandler.similiar(
+        verticalPadding: 110,
+        horizontalPadding: 38,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Column(
+                  children: [
+                    StepByStep(),
+                    Text(
+                      "Conta",
+                      style: AppText.header,
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 78),
+                  child: Column(
+                    children: [
+                      FormattedTextField(
+                        hintText: "E-mail",
+                      ),
+                      SizedBox(
+                        height: textFieldsGap,
+                      ),
+                      FormattedTextField(
+                        hintText: "Palavra-chave",
+                        isPassword: true,
+                      ),
+                      SizedBox(
+                        height: textFieldsGap,
+                      ),
+                      FormattedTextField(
+                        hintText: "Confirmar palavra-chave",
+                        isPassword: true,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            FormattedButton(content: "Concluir")
+          ],
+        ));
+  }
+}

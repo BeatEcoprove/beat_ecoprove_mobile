e5bd6038aa827e11ecb4d0f2ebc92271ab06fa79
David <davidjosebraga@gmail.com>
2023-11-13 23:30:37 +0000

    refactor: make info_cloth_view receive information from data file

--- lib/default_layout/presentation/default_layout_view.dart
+++ lib/default_layout/presentation/default_layout_view.dart
@@ -1,5 +1,6 @@
 import 'package:beat_ecoprove/clothing/presentation/clothing_view.dart';
 import 'package:beat_ecoprove/clothing/presentation/info_cloth/info_cloth_view.dart';
+import 'package:beat_ecoprove/core/config/data.dart';
 import 'package:beat_ecoprove/core/widgets/footer/footer.dart';
 import 'package:beat_ecoprove/core/widgets/headers/header.dart';
 import 'package:beat_ecoprove/home/presentation/index/home_view.dart';
@@ -15,8 +16,10 @@ class DefaultLayoutView extends StatelessWidget {
     return Scaffold(
       appBar: header,
       body: PageView(
-        children: const [
-          InfoClothView(),
+        children: [
+          InfoClothView(
+            card: clothItems[2].toCardItem(),
+          ),
         ],
       ),
       bottomNavigationBar: const Footer(),

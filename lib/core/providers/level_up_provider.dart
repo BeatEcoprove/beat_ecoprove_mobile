0424495057a42709a8a209a29b1c9180622e1542
David <davidjosebraga@gmail.com>
2024-01-26 02:31:43 +0000

    add: content to level up modal

--- lib/core/providers/level_up_provider.dart
+++ lib/core/providers/level_up_provider.dart
@@ -1,5 +1,7 @@
+import 'package:beat_ecoprove/core/config/global.dart';
 import 'package:beat_ecoprove/core/view_model.dart';
 import 'package:beat_ecoprove/core/widgets/overlay_widget_with_button.dart';
+import 'package:beat_ecoprove/core/widgets/svg_image.dart';
 import 'package:flutter/material.dart';
 
 class LevelUpProvider extends ViewModel {
@@ -32,10 +34,85 @@ class LevelUpProvider extends ViewModel {
     );
   }
 
-  Widget _content() {
-    return Container(
-      height: 200,
-      color: Colors.black,
+  //TODO: Change
+  Widget _content({int level = 10}) {
+    const double ratioOffset = 80;
+    const double heightStack = 175;
+    const double widthStack = 220;
+
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Positioned(
+              child: Container(
+                height: heightStack,
+                width: widthStack,
+              ),
+            ),
+            const Positioned(
+              left: 0,
+              child: SvgImage(
+                height: 150,
+                width: 150,
+                path: 'assets/level_up/leaf1.svg',
+              ),
+            ),
+            const Positioned(
+              right: 0,
+              child: SvgImage(
+                height: 150,
+                width: 150,
+                path: 'assets/level_up/leaf2.svg',
+              ),
+            ),
+            const Positioned(
+              top: -5,
+              left: widthStack / 2 - 20,
+              child: SvgImage(
+                height: 50,
+                width: 50,
+                path: 'assets/level_up/crown.svg',
+              ),
+            ),
+            Positioned(
+              bottom: heightStack / 2 - 40,
+              left: widthStack / 2 - 40,
+              child: ClipOval(
+                child: Container(
+                  width: ratioOffset,
+                  height: ratioOffset,
+                  decoration: const BoxDecoration(
+                      color: AppColor.widgetBackground,
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                      boxShadow: [AppColor.defaultShadow]),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Nível",
+                        style: AppText.smallSubHeader,
+                      ),
+                      Text(
+                        level.toString(),
+                        style: AppText.smallHeader,
+                      ),
+                      const Text(
+                        "0%",
+                        style: AppText.percentText,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+      ],
     );
   }
 }
--- lib/profile/presentation/profile/profile_view_model.dart
+++ lib/profile/presentation/profile/profile_view_model.dart
@@ -1,17 +1,17 @@
 import 'package:beat_ecoprove/core/domain/entities/user.dart';
 import 'package:beat_ecoprove/core/providers/auth/authentication_provider.dart';
-import 'package:beat_ecoprove/core/providers/level_up_provider.dart';
 import 'package:beat_ecoprove/core/view_model.dart';
 import 'package:go_router/go_router.dart';
 
 class ProfileViewModel extends ViewModel {
-  final LevelUpProvider _levelUpProvider;
   final AuthenticationProvider _authProvider;
   final GoRouter _navigationRouter;
   late final User _user;
 
   ProfileViewModel(
-      this._levelUpProvider, this._authProvider, this._navigationRouter) {
+    this._authProvider,
+    this._navigationRouter,
+  ) {
     _user = _authProvider.appUser;
   }
 
@@ -19,9 +19,5 @@ class ProfileViewModel extends ViewModel {
 
   void settings() {
     _navigationRouter.push('/settings');
-  }
-
-  void levelUp() {
-    _levelUpProvider.showLevelUpNotification();
   }
 }

2b08e7f591f327db81ea640d3e5c3d16adbcc134
David <davidjosebraga@gmail.com>
2024-01-29 03:08:25 +0000

    refactor: fix some data receive by server

--- lib/register_cloth/contracts/brand_result.dart
+++ lib/register_cloth/contracts/brand_result.dart
@@ -1,13 +1,16 @@
 class BrandResult {
   final String name;
+  final String brandAvatar;
 
   BrandResult(
     this.name,
+    this.brandAvatar,
   );
 
   factory BrandResult.fromJson(Map<String, dynamic> json) {
     return BrandResult(
       json['name'],
+      json['brandAvatar'],
     );
   }
 }

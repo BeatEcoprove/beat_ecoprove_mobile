07942bf411fc21a7db9c6bf3fd622b498c99bb13
David Braga <28248.David@gmail.com>
2024-05-04 23:07:31 +0100

    add: options to chat message

--- lib/core/widgets/chat/chat_item_root.dart
+++ lib/core/widgets/chat/chat_item_root.dart
@@ -1,4 +1,5 @@
 import 'package:beat_ecoprove/core/config/global.dart';
+import 'package:beat_ecoprove/core/domain/models/optionItem.dart';
 import 'package:beat_ecoprove/core/widgets/chat/chat_item.dart';
 import 'package:beat_ecoprove/core/widgets/present_image.dart';
 import 'package:beat_ecoprove/core/widgets/server_image.dart';
@@ -12,30 +13,19 @@ class ChatItemRoot extends StatelessWidget {
   final List<ChatListItem> items;
 
   final VoidCallback? click;
-  final VoidCallback? onPress;
+  final List<OptionItem>? options;
 
-  const ChatItemRoot({
+  final GlobalKey buttonKey = GlobalKey();
+
+  ChatItemRoot({
     super.key,
     required this.userIsSender,
     required this.avatarUrl,
     required this.createdAt,
     required this.items,
     this.click,
-    this.onPress,
+    this.options,
   });
-
-  @override
-  Widget build(BuildContext context) {
-    if (click != null) {
-      return InkWell(
-        onTap: click,
-        onLongPress: onPress,
-        child: body(context),
-      );
-    }
-
-    return body(context);
-  }
 
   Widget _avatar(double imageSize) {
     return SizedBox(
@@ -98,5 +88,41 @@ class ChatItemRoot extends StatelessWidget {
         children: _content(maxWidth),
       ),
     );
+  }
+
+  void _options(BuildContext context) {
+    final RenderBox buttonRenderBox =
+        buttonKey.currentContext!.findRenderObject() as RenderBox;
+    final Offset buttonPosition = buttonRenderBox.localToGlobal(Offset.zero);
+
+    showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(
+        buttonPosition.dx,
+        buttonPosition.dy,
+        buttonPosition.dx + buttonRenderBox.size.width,
+        buttonPosition.dy + buttonRenderBox.size.height,
+      ),
+      items: options!.map((option) {
+        return PopupMenuItem(
+          onTap: option.action,
+          child: Text(option.name),
+        );
+      }).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (click != null || options != null) {
+      return InkWell(
+        key: buttonKey,
+        onTap: click,
+        onLongPress: options == null ? () => {} : () => _options(context),
+        child: body(context),
+      );
+    }
+
+    return body(context);
   }
 }

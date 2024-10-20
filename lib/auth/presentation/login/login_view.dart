369065ec2f2e4f5da5d2bb1c8acb0538e3b41bec
David-Ogrande <davidjosebraga@gmail.com>
2024-10-20 22:35:23 +0100

    add: deny go back in certain pages using the button on the phone

--- lib/auth/presentation/login/login_view.dart
+++ lib/auth/presentation/login/login_view.dart
@@ -23,107 +23,110 @@ class LoginView extends LinearView<LoginViewModel> {
 
   @override
   Widget build(BuildContext context, LoginViewModel viewModel) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: AppBackground(
-        type: AppBackgrounds.login,
-        content: ScrollHandler(
-          topPadding: _topPadding,
-          bottomPadding: _bottomPadding,
-          leftPadding: _horizontalPadding,
-          rightPadding: _horizontalPadding,
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const SvgImage(
-                  path: "assets/applicationTitle.svg",
-                  height: 74.23,
-                  width: 224,
-                ),
-                Column(
-                  children: [
-                    DefaultFormattedTextField(
-                      hintText: LocaleContext.get().auth_login_email,
-                      leftIcon: const Icon(
-                        Icons.email_outlined,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: AppBackground(
+          type: AppBackgrounds.login,
+          content: ScrollHandler(
+            topPadding: _topPadding,
+            bottomPadding: _bottomPadding,
+            leftPadding: _horizontalPadding,
+            rightPadding: _horizontalPadding,
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const SvgImage(
+                    path: "assets/applicationTitle.svg",
+                    height: 74.23,
+                    width: 224,
+                  ),
+                  Column(
+                    children: [
+                      DefaultFormattedTextField(
+                        hintText: LocaleContext.get().auth_login_email,
+                        leftIcon: const Icon(
+                          Icons.email_outlined,
+                        ),
+                        initialValue:
+                            viewModel.getValue(FormFieldValues.email).value,
+                        errorMessage:
+                            viewModel.getValue(FormFieldValues.email).error,
+                        onChange: (value) => viewModel.setEmail(value),
                       ),
-                      initialValue:
-                          viewModel.getValue(FormFieldValues.email).value,
-                      errorMessage:
-                          viewModel.getValue(FormFieldValues.email).error,
-                      onChange: (value) => viewModel.setEmail(value),
-                    ),
-                    const SizedBox(height: 16),
-                    DefaultFormattedTextField(
-                      isPassword: !viewModel.isPassword,
-                      hintText: LocaleContext.get().auth_login_password,
-                      leftIcon: const Icon(
-                        Icons.lock_outline,
-                        color: AppColor.widgetSecondary,
+                      const SizedBox(height: 16),
+                      DefaultFormattedTextField(
+                        isPassword: !viewModel.isPassword,
+                        hintText: LocaleContext.get().auth_login_password,
+                        leftIcon: const Icon(
+                          Icons.lock_outline,
+                          color: AppColor.widgetSecondary,
+                        ),
+                        controller: viewModel.passwordTextController,
+                        initialValue:
+                            viewModel.getValue(FormFieldValues.password).value,
+                        errorMessage:
+                            viewModel.getValue(FormFieldValues.password).error,
+                        onChange: (value) => viewModel.setPassword(value),
                       ),
-                      controller: viewModel.passwordTextController,
-                      initialValue:
-                          viewModel.getValue(FormFieldValues.password).value,
-                      errorMessage:
-                          viewModel.getValue(FormFieldValues.password).error,
-                      onChange: (value) => viewModel.setPassword(value),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: FormattedCheckBox(
-                        value: viewModel.isPassword,
-                        onChanged: viewModel.setPasswordVisibitlity,
-                        title: "Mostrar palavra-chave",
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: FormattedCheckBox(
+                          value: viewModel.isPassword,
+                          onChanged: viewModel.setPasswordVisibitlity,
+                          title: "Mostrar palavra-chave",
+                        ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 16, horizontal: 8),
-                        child: GestureDetector(
-                          onTap: () async =>
-                              await viewModel.handleForgotPassword(),
-                          child: Text(
-                            LocaleContext.get().auth_login_forgot_password,
-                            style: AppText.underlineStyle,
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 8),
+                          child: GestureDetector(
+                            onTap: () async =>
+                                await viewModel.handleForgotPassword(),
+                            child: Text(
+                              LocaleContext.get().auth_login_forgot_password,
+                              style: AppText.underlineStyle,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                FormattedButton(
-                  content: LocaleContext.get().auth_login_sign_in,
-                  textColor: Colors.white,
-                  onPress: () async {
-                    viewModel.handleLogin();
-                  },
-                  disabled: viewModel.thereAreErrors,
-                  loading: viewModel.isLoading,
-                ),
-                // Footer
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(LocaleContext.get().auth_login_got_no_account,
-                          style: AppText.strongStyle),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      GestureDetector(
-                        onTap: () => viewModel.handleSignUp(),
-                        child: Text(
-                          LocaleContext.get().auth_login_sign_up,
-                          style: AppText.underlineStyle,
-                        ),
-                      )
                     ],
                   ),
-                )
-              ],
+                  FormattedButton(
+                    content: LocaleContext.get().auth_login_sign_in,
+                    textColor: Colors.white,
+                    onPress: () async {
+                      viewModel.handleLogin();
+                    },
+                    disabled: viewModel.thereAreErrors,
+                    loading: viewModel.isLoading,
+                  ),
+                  // Footer
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(LocaleContext.get().auth_login_got_no_account,
+                            style: AppText.strongStyle),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        GestureDetector(
+                          onTap: () => viewModel.handleSignUp(),
+                          child: Text(
+                            LocaleContext.get().auth_login_sign_up,
+                            style: AppText.underlineStyle,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/core/presentation/no_wifi/no_wifi_view.dart
+++ lib/core/presentation/no_wifi/no_wifi_view.dart
@@ -15,54 +15,57 @@ class NoWifiView extends LinearView<NoWifiViewModel> {
   Widget build(BuildContext context, NoWifiViewModel viewModel) {
     double gapHeight = (MediaQuery.of(context).size.height / 4) - 24;
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: AppBackground(
-          content: SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-            child: Column(
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 26),
-                  child: Center(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: gapHeight,
-                        ),
-                        SizedBox(
-                          height: gapHeight * 2,
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: (gapHeight / 3) - 16,
-                              ),
-                              const Text(
-                                "Sem Internet!",
-                                style: AppText.header,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              const SvgImage(
-                                path: "assets/nowifi.svg",
-                                height: 172,
-                                width: 172,
-                              ),
-                            ],
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: AppBackground(
+            content: SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 26),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: gapHeight,
                           ),
-                        ),
-                        SizedBox(
-                          height: gapHeight,
-                        ),
-                      ],
+                          SizedBox(
+                            height: gapHeight * 2,
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: (gapHeight / 3) - 16,
+                                ),
+                                const Text(
+                                  "Sem Internet!",
+                                  style: AppText.header,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SvgImage(
+                                  path: "assets/nowifi.svg",
+                                  height: 172,
+                                  width: 172,
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: gapHeight,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          type: AppBackgrounds.login),
+            type: AppBackgrounds.login),
+      ),
     );
   }
 }
